b733d724edb9c46653bad651d390bf0b6f7d24bd
Adam Linscott <13459186@students.lincoln.ac.uk>
2016-12-04 21:14:04 +0000

    Deserialising JSON

---
+++ TimeTableCalculator/TimeTableCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json;
+using System.IO;
+using System.Reflection;
+
+namespace TimeTableCalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Get the current directory.
+			string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\"));
+			Console.WriteLine("Current dir: " + path);
+
+			string JSONString = System.IO.File.ReadAllText(path + @"input.json");
+			var JSONObject = JsonParser.Deserialize(JSONString);
+
+			Console.ReadKey();
+		}
+	}
+}
